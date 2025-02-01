208050d808d6a2a4744adc862db0937f57361dd3
Sugandhamiddha04 <195461520+Sugandhamiddha04@users.noreply.github.com>
2025-02-02 00:32:39 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@
 int main() {
     int num1,num2;
     char op;
-    scanf("%d""%d""%c" , &num1, &num2, &op);
+    scanf("%d %d %c" , &num1, &num2, &op);
         
     switch(op) {
         case '+':
